6f3375a383cb47962adf70a710454db397481de7
Sueda Tekin <115072432+SuedaTekin@users.noreply.github.com>
2023-06-15 21:44:59 +0300

    Add files via upload

---
+++ Function/example59.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<string.h>
+int main(){
+	char src[50],
+	dest[50];
+	strcpy(src,"this is source");
+	strcpy(dest,"this is destination");
+	strcat(dest,src);
+	printf("Final destination string:|%s|",dest);
+	return(0);
+	
+}
